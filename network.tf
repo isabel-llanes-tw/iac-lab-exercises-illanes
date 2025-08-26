94dc1bff6b18d2d7f74f418d92fa63fba28226dc
isabel-llanes <isabel.llanes@easyjet.com>
2025-08-26 19:53:13 +0200

    Add subnets

---
+++ network.tf
@@ -0,0 +1,73 @@
+resource "aws_vpc" "iac_lab_vpc" {
+  cidr_block = var.vpc_cidr
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = format("%s-vpc", var.prefix)
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.iac_lab_vpc.id
+  cidr_block              = var.subnet1_cidr
+  availability_zone       = format("%sa", var.region)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = format("%s-public-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.iac_lab_vpc.id
+  cidr_block              = var.subnet2_cidr
+  availability_zone       = format("%sa", var.region)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = format("%s-public-subnet-2", var.prefix)
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.iac_lab_vpc.id
+  cidr_block        = var.subnet3_cidr
+  availability_zone = format("%sa", var.region)
+
+  tags = {
+    Name = format("%s-private-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.iac_lab_vpc.id
+  cidr_block        = var.subnet4_cidr
+  availability_zone = format("%sa", var.region)
+
+  tags = {
+    Name = format("%s-private-subnet-2", var.prefix)
+  }
+}
+
+resource "aws_subnet" "secure_subnet_1" {
+  vpc_id            = aws_vpc.iac_lab_vpc.id
+  cidr_block        = var.subnet5_cidr
+  availability_zone = format("%sa", var.region)
+
+  tags = {
+    Name = format("%s-secure-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "secure_subnet_2" {
+  vpc_id            = aws_vpc.iac_lab_vpc.id
+  cidr_block        = var.subnet6_cidr
+  availability_zone = format("%sb", var.region)
+
+  tags = {
+    Name = format("%s-secure-subnet-2", var.prefix)
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "prefix" {
+  type        = string
+  description = "Prefix for naming AWS resources."
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region."
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC."
+}
+
+variable "subnet1_cidr" {
+  type        = string
+  description = "CIDR block for first subnet."
+}
+
+variable "subnet2_cidr" {
+  type        = string
+  description = "CIDR block for second subnet."
+}
+
+variable "subnet3_cidr" {
+  type        = string
+  description = "CIDR block for third subnet."
+}
+
+variable "subnet4_cidr" {
+  type        = string
+  description = "CIDR block for fourth subnet."
+}
+
+variable "subnet5_cidr" {
+  type        = string
+  description = "CIDR block for fifth subnet."
+}
+
+variable "subnet6_cidr" {
+  type        = string
+  description = "CIDR block for sixth subnet."
+}
